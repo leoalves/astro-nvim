38cb9ad92b8ee0a1d70ac509be19c9b61cfca4bd
Leonardo Alves <hello@leoalves.net>
2025-05-20 16:35:08 -0400

    removed typscript lsp, already added as community package

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -11,8 +11,6 @@ return {
       ensure_installed = {
         -- install language servers
         "lua-language-server",
-        "typescript-language-server",
-        "tailwindcss-language-server",
 
         -- install formatters
         "stylua",
