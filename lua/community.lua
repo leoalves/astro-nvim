8a3ac0ac4f456d1a92057d0c0b7de3086c625a17
Leonardo Alves <hello@leoalves.net>
2025-05-20 16:08:28 -0400

    typescript lsp

--- lua/community.lua
+++ lua/community.lua
@@ -5,6 +5,7 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
